cba0ad0e335393939c87c4c7acf5706a79cbc389
Leonardl00 <leonard.louis88@gmail.com>
2022-06-06 16:33:33 +0800

    add admin user list paginate

--- resources/views/admin-user.blade.php
+++ resources/views/admin-user.blade.php
@@ -109,11 +109,7 @@
                     <!-- /User Listing Table-->
                     <nav>
                         <ul class="pagination justify-content-end mt-3 mb-0">
-                          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                          <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                          <li class="page-item"><a class="page-link" href="#">2</a></li>
-                          <li class="page-item"><a class="page-link" href="#">3</a></li>
-                          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            {!! $users->links() !!}
                         </ul>
                       </nav>
                 </div>
